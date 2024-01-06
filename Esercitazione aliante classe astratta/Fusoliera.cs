26c50b3288791eea677fdc1bfdf0764c9dfb476a
ezioc <ezioc@DESKTOP-OIS0K39>
2024-01-06 16:25:15 +0100

    Classe Fusoliera completata

--- Esercitazione aliante classe astratta/Fusoliera.cs
+++ Esercitazione aliante classe astratta/Fusoliera.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace Esercitazione_aliante_classe_astratta
 {
-    public class Fusoliera
+    public class Fusoliera : Composite
     {
+        private double _lung;
+        private string _materiale;
+
+        public double Lung
+        {
+            get { return _lung;}
+            set { _lung = value;}
+        }
+
+        public string Materiale
+        {
+            get { return _materiale; }
+            set { _materiale = value; }
+        }
+
+        public Fusoliera()
+        {
+            Lung = 0;
+            Materiale = string.Empty;
+        }
+
+        public Fusoliera(double lung, string materiale)
+        {
+            Lung = lung;
+            Materiale = materiale;
+        }
+
+        public Fusoliera(Fusoliera vFusoliera)
+        {
+            Lung = vFusoliera.Lung;
+            Materiale = vFusoliera.Materiale;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Fusoliera) || obj == null)
+            {
+                return false;
+            }
+
+            Fusoliera altro = (Fusoliera)obj;
+            return Lung == altro.Lung && Materiale == altro.Materiale;
+        }
+
+        public override void Aggiunta(Composite component)
+        {
+
+        }
+
+        public override void Rimuovi(int index)
+        {
+
+        }
+
+        public override Composite GetChild(int index)
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return $"Lunghezza fusoliera: {Lung}; Materiale fusoliera: {Materiale}";
+        }
+
+        public override double Prezzo()
+        {
+            return Lung * 30;
+        }
     }
 }
